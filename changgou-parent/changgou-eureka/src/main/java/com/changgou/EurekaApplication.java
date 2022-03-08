3c199d3cb3bdc124c5c5508c3c7b9d446f1be368
xqls-ng <1573799163@qq.com>
2022-03-09 03:17:29 +0800

    搭建框架

---
+++ changgou-parent/changgou-eureka/src/main/java/com/changgou/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.changgou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author 小桥流水
+ * @description 功能描述
+ * @date 2022-03-09 3:10
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
